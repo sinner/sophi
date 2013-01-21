4ab36c6f393854adb87cc45301044f861e668b21
José Gabriel González Pérez <jgabrielsinner10@gmail.com>
2013-01-20 22:23:35 -0430

    Sustitución de la plantilla por defecto de login de FOSUserBundle.

--- src/Sophi/UserBundle/Controller/DefaultController.php
+++ src/Sophi/UserBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('SophiUserBundle:Default:index.html.twig', array('name' => $name));
+        //return $this->render('SophiUserBundle:Default:index.html.twig', array('name' => $name));
     }
 }
