f6e34c90c458691cee5502a822ac4895f82fd353
José Gabriel González Pérez <jgabrielsinner10@gmail.com>
2013-02-07 07:14:49 -0430

    Arreglando unos estilos en el login.

--- src/Sophi/CoreBundle/Resources/public/js/custom.js
+++ src/Sophi/CoreBundle/Resources/public/js/custom.js
@@ -199,7 +199,7 @@ function template_functions(){
 	});
 
 	/* ---------- Uniform ---------- */
-	$("input:checkbox, input:radio, input:file").not('[data-no-uniform="true"],#uniform-is-ajax').uniform();
+	//$("input:checkbox, input:radio, input:file").not('[data-no-uniform="true"],#uniform-is-ajax').uniform();
 
 	/* ---------- Choosen ---------- */
 	$('[data-rel="chosen"],[rel="chosen"]').chosen();
