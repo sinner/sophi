b38041db786cd37b482266bf129165e090b7e5f1
Jose Gabriel Gonzalez Perez <jgabrielsinner10@gmail.com>
2013-05-05 09:35:37 -0430

    Empezando el Proyecto de nuevo con Symfony2 versión 2.3.0BETA1

--- src/Sophi/CoreBundle/SophiCoreBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Sophi\CoreBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SophiCoreBundle extends Bundle
-{
-}
--- symfony.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-php app/console  %*
